52f52c931bc99f4894b8b03c9475c39e02da2035
Mykhailo Vavrykovych <vavr.mykhailo@gmail.com>
2021-09-29 17:16:18 +0300

    fix prog_3

--- programming/prog_3/app.py
+++ programming/prog_3/app.py
@@ -1,10 +1,25 @@
 from programming.prog_3.mainClasses.hotel import Hotel
 from programming.prog_3.supportClasses.collection import Collection
-from programming.prog_3.supportClasses.validation import Validation
+from programming.prog_3.supportClasses.funcForOptions import FuncForOptions
+
 
 # use for input "C:/python_learn_lnu/programming/prog_3/data/testData.json"
 # use for output "C:/python_learn_lnu/programming/prog_3/data/outputData.json"
 # use to fill collection "C:/python_learn_lnu/programming/prog_3/data/data.json"
+
+
+def options(option, collection):
+    functions = FuncForOptions(collection)
+    return {
+        "1": functions.f1,
+        "2": functions.f2,
+        "3": functions.f3,
+        "4": functions.f4,
+        "5": functions.f5,
+        "6": functions.f6,
+        "0": functions.f7,
+    }.get(option)
+
 
 def start(collection):
     print("""Choose the option:
@@ -17,50 +32,10 @@ def start(collection):
     0. Exit
     """)
 
-    def getIdAndPath(methodName):
-        print(f"Id for {methodName}:")
-        id = input()
-        Validation.isInteger(id)
-        id = int(id)
-        Validation.isInRange(id, 0)
-        return id
-
-    def getPath(isInPath=False, isOutPath=False):
-        inPath, outPath = "", ""
-        if isInPath:
-            print("Write path to input data:")
-            inPath = input()
-        if isOutPath:
-            print("Write path to output data:")
-            outPath = input()
-        return inPath, outPath
-
-
     option = input()
-
-    if option == '1':
-        print("String for find:")
-        str = input()
-        collection.find(str)
-    elif option == '2':
-        print("Attribute for sort:")
-        attr = input()
-        collection.sort(attr)
-    elif option == '3':
-        id = getIdAndPath("delete")
-        inPath, outPath = getPath()
-        collection.delete(id, outPath)
-    elif option == '4':
-        inPath, outPath = getPath(True)
-        collection.add(inPath, outPath)
-    elif option == '5':
-        id = getIdAndPath("change")
-        inPath, outPath = getPath(True)
-        collection.change(id, inPath, outPath)
-    elif option == '6':
-        collection.show()
-    elif option == '0':
-        exit()
+    method = options(option, collection)
+    if method:
+        method()
     else:
         print("Choose correct option")
         start(collection)
